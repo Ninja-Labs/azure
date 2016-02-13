9fad2529971804447546a4d86813dd8ab1487887
Sorey Bibiana García Zapata <soreygarcia@hotmail.com>
2016-02-13 00:20:30 -0500

    Se sube lab y codigo del lab con diseño adaptativo y navigation pattern

---
+++ 7. Apps Service and Universal Windows Platform/Lab 1/WorldCupDevCamp/WorldCupDevCamp/ViewModels/ShellViewModel.cs
@@ -0,0 +1,74 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
+
+namespace WorldCupDevCamp.ViewModels
+{
+    public class ShellViewModel : ViewModelBase
+    {
+        public ShellViewModel(Frame rootFrame)
+        {
+            this.RootFrame = rootFrame;
+
+            MenuItems.Add(new MenuItemViewModel(rootFrame) { Icon = Symbol.OtherUser, Title = "Equipos", PageType = typeof(TeamsPage) });
+            MenuItems.Add(new MenuItemViewModel(rootFrame) { Icon = Symbol.Add, Title = "Crear Equipo", PageType = typeof(CreateTeamPage) });
+            MenuItems.Add(new MenuItemViewModel(rootFrame) { Icon = Symbol.Shuffle, Title = "Grupos", PageType = typeof(GroupsPage) });
+
+            SelectedMenuItem = MenuItems.First();
+        }
+
+        public ObservableCollection<MenuItemViewModel> MenuItems { get; set; } = new ObservableCollection<MenuItemViewModel>();        
+
+        private bool isSplitViewPaneOpen;
+
+        public bool IsSplitViewPaneOpen
+        {
+            get { return this.isSplitViewPaneOpen; }
+            set { Set(ref this.isSplitViewPaneOpen, value); }
+        }
+
+        private MenuItemViewModel selectedMenuItem;
+
+        public MenuItemViewModel SelectedMenuItem
+        {
+            get { return this.selectedMenuItem; }
+            set
+            {
+                if (Set(ref this.selectedMenuItem, value))
+                {
+                    RaisePropertyChanged("SelectedPageType");
+                    this.IsSplitViewPaneOpen = false;
+                }
+            }
+        }
+
+        public Type SelectedPageType
+        {
+            get
+            {
+                return this.selectedMenuItem?.PageType;
+            }
+            set
+            {
+                this.SelectedMenuItem = this.MenuItems.FirstOrDefault(m => m.PageType == value);
+            }
+        }
+
+        public ICommand ToggleSplitViewPaneCommand
+        {
+            get
+            {
+                return new RelayCommand(() => this.IsSplitViewPaneOpen = !this.IsSplitViewPaneOpen);
+            }
+        }
+
+        public Frame RootFrame { get; private set; }
+    }
+}
---
+++ 7. Apps Service and Universal Windows Platform/Lab 1/WorldCupDevCamp/WorldCupDevCamp/ViewModels/TeamViewModel.cs
@@ -0,0 +1,16 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldCupDevCamp.ViewModels
+{
+    public class TeamViewModel : ViewModelBase
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Flag { get; set; }
+    }
+}
