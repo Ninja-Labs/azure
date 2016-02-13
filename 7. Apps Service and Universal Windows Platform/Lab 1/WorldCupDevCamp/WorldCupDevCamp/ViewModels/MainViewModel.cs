2c9854917aa14cfafa2e65302b74343c5d03e9fb
Sorey Bibiana García Zapata <soreygarcia@hotmail.com>
2016-02-13 02:30:26 -0500

    Distribución de grupos con random simple

--- 7. Apps Service and Universal Windows Platform/Lab 1/WorldCupDevCamp/WorldCupDevCamp/ViewModels/MainViewModel.cs
+++ 7. Apps Service and Universal Windows Platform/Lab 1/WorldCupDevCamp/WorldCupDevCamp/ViewModels/MainViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using WorldCupDevCamp.Services;
 
 namespace WorldCupDevCamp.ViewModels
@@ -35,6 +37,26 @@ namespace WorldCupDevCamp.ViewModels
             {
                 this.Teams.Add(item);
             }
+        }
+
+        public ICommand AssignGroupsCommand
+        {
+            get
+            {
+                return new RelayCommand(AssignGroups);
+            }
+        }
+
+        private void AssignGroups()
+        {
+            Random rand = new Random();
+
+            foreach (var item in this.Teams)
+            {
+                item.Group = "Grupo " + rand.Next(1, 5);
+            }
+
+            RaisePropertyChanged("Teams");
         }
     }
 }
